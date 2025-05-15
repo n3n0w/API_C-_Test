b6412cbf1fa0038603a6e487cf340e94d6c5e7cf
n3n0w <nenovkrasi@gmail.com>
2025-05-15 14:23:35 +0300

    update

--- src/PU-Examples/PU-Examples/obj/Debug/net8.0/PU-Examples.AssemblyInfo.cs
+++ src/PU-Examples/PU-Examples/obj/Debug/net8.0/PU-Examples.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PU-Examples")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+307dbe44f643338256323c7694a73d575f1d8a11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+605ff7fb4f59e8749b38ee3f4f763752c38fb075")]
 [assembly: System.Reflection.AssemblyProductAttribute("PU-Examples")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PU-Examples")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
